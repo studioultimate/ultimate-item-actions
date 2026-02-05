ddc37f44b066b1ec5c35d144fdc71bdf8b576346
gabriaum <gaabrielbruck@gmail.com>
2026-02-04 20:57:16 -0800

    fix: checking if the key is null when collecting UltimateItemActions

--- src/main/java/com/gabriaum/ultimate/itemactions/UltimateItemActionsRegistry.java
+++ src/main/java/com/gabriaum/ultimate/itemactions/UltimateItemActionsRegistry.java
@@ -2,8 +2,15 @@ package com.gabriaum.ultimate.itemactions;
 
 public class UltimateItemActionsRegistry {
     public static UltimateItemActions getByKey(String key) {
-        return UltimateItemActionsMain.getInstance().getManager().stream()
-                .filter(item -> item.getKey().equals(key))
+        if (key == null) return null;
+
+        return UltimateItemActionsMain.getInstance()
+                .getManager()
+                .stream()
+                .filter(item ->
+                        item.getKey() != null &&
+                                item.getKey().equalsIgnoreCase(key)
+                )
                 .findFirst()
                 .orElse(null);
     }
