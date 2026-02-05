d67e32d755a7284a42f7a91494b5abf2bc2d148d
gabriaum <gaabrielbruck@gmail.com>
2026-02-04 18:41:18 -0800

    feat: manager to cache item actions

--- src/main/java/com/gabriaum/ultimate/itemactions/UltimateItemActionsMain.java
+++ src/main/java/com/gabriaum/ultimate/itemactions/UltimateItemActionsMain.java
@@ -1,6 +1,7 @@
 package com.gabriaum.ultimate.itemactions;
 
 import com.gabriaum.ultimate.itemactions.infra.service.ConsoleService;
+import com.gabriaum.ultimate.itemactions.manager.UltimateItemActionsManager;
 import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -8,6 +9,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class UltimateItemActionsMain extends JavaPlugin {
     @Getter
     protected static UltimateItemActionsMain instance;
+
+    private UltimateItemActionsManager manager;
 
     @Override
     public void onLoad() {
@@ -18,7 +21,13 @@ public class UltimateItemActionsMain extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        loadManagers();
+
         ConsoleService.log("Apparently no problems were found and the plugin was started.");
+    }
+
+    private void loadManagers() {
+        manager = new UltimateItemActionsManager();
     }
 
     @Override
---
+++ src/main/java/com/gabriaum/ultimate/itemactions/manager/UltimateItemActionsManager.java
@@ -0,0 +1,7 @@
+package com.gabriaum.ultimate.itemactions.manager;
+
+import com.gabriaum.ultimate.itemactions.UltimateItemActions;
+
+import java.util.ArrayList;
+
+public class UltimateItemActionsManager extends ArrayList<UltimateItemActions> {}
